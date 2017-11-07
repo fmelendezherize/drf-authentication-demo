50cd5cba0795c92111ba7527f605ae097548ae17
FranciscoMH <fmelendezherize@gmail.com>
2017-11-07 17:53:24 -0400

    Serializers and Views done

--- drf-authentication-demo/authentication/urls.py
+++ drf-authentication-demo/authentication/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import url
-from rest_framework_jwt.views import obtain_jwt_token
+from .views import AuthLogin, AuthRegister, AccountList
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 
 urlpatterns = [
     url(r'^login/', obtain_jwt_token),
+    url(r'^token-refresh/', refresh_jwt_token),
+    url(r'^token-verify/', verify_jwt_token),
+    url(r'^register/$', AuthRegister.as_view()),
+    url(r'^accounts/$', AccountList.as_view())
 ]
--- drf-authentication-demo/authentication/views.py
+++ drf-authentication-demo/authentication/views.py
@@ -1,3 +1,55 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializers import AccountSerializer
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework import status
+
+from django.contrib.auth import login, authenticate
+from .models import Account
 
 # Create your views here.
+class AccountList(APIView):
+    serializer_class = AccountSerializer
+    permission_classes = (AllowAny,)
+
+    def get(self, request, format=None):
+        accounts = Account.objects.all()
+        #Must have its own serializer
+        serializer = self.serializer_class(accounts, many=True)
+        return Response(serializer.data)
+
+class AuthRegister(APIView):
+    """
+    Register a new user.
+    """
+    serializer_class = AccountSerializer
+    permission_classes = (AllowAny,)
+
+    def post(self, request, format=None):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class AuthLogin(APIView):
+    ''' Manual implementation of login method '''
+    def post(self, request, format=None):
+        data = request.data
+        email = data.get('email', None)
+        password = data.get('password', None)
+
+        account = authenticate(email=email, password=password)
+        # Generate token and add it to the response object
+        if account is not None:
+            login(request, account)
+            return Response({
+                'status': 'Successful',
+                'message': 'You have successfully been logged into your account.'
+            }, status=status.HTTP_200_OK)
+
+        return Response({
+            'status': 'Unauthorized', 
+            'message': 'Username/password combination invalid.'
+            }, status=status.HTTP_401_UNAUTHORIZED)
